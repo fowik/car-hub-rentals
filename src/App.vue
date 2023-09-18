666539a831354336bef986bd6f7cc4fd17030a34
X <ilarimsa937@gmail.com>
2023-09-19 01:24:55 +0300

    transferred to vue.cli

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <NavBar />
+  <router-view />
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  },
+  data () { 
+    return {
+      title: 'Welcome to Car Hub Rentals'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,60 @@
+<template>
+    
+    <nav class="navbar">
+        <div class="logo">
+            <a href="/"><h1>Car<span class="orange-box">Hub</span></h1></a>
+        </div>
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Cars</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+    </nav>
+
+</template>
+
+<style scoped>
+.navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #292929;
+    padding: 20px;
+}
+
+.logo h1 {
+    margin: 0;
+    font-size: 28px;
+    color: #ffffff; 
+}
+
+.logo .orange-box {
+    background-color: #ffa31a;
+    padding: 5px;
+    border-radius: 5%;
+    color: #1b1b1b;
+    margin-left: 5px;
+}
+
+.nav-links {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+}
+
+.nav-links li {
+    margin-right: 20px;
+}
+
+.nav-links a {
+    text-decoration: none;
+    color: #ffffff;
+    font-weight: bold;
+}
+
+.nav-links a:hover {
+    text-decoration: underline;
+}
+</style>
