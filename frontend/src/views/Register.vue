fa9cd8695d173953299f23b0a626cf2890808f60
Ilja Rimša <ilarimsa937@gmail.com>
2023-10-04 00:55:20 +0300

    fixed response error

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -44,22 +44,32 @@ export default {
                 password: this.password
             };
 
-            await fetch('http://localhost:3000/api/users/register', {
-                method: "POST", // *GET, POST, PUT, DELETE, etc.
-                mode: "cors", // no-cors, *cors, same-origin
-                cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: "same-origin", // include, *same-origin, omit
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                redirect: "follow", // manual, *follow, error
-                referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-                body: JSON.stringify(userData), // body data type must match "Content-Type" header
+            try {
+                const res = await fetch('http://localhost:3000/api/users/register', {
+                    method: "POST", // *GET, POST, PUT, DELETE, etc.
+                    mode: "cors", // no-cors, *cors, same-origin
+                    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                    credentials: "same-origin", // include, *same-origin, omit
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    redirect: "follow", // manual, *follow, error
+                    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                    body: JSON.stringify(userData), // body data type must match "Content-Type" header
+                });
 
-                // return to login page
-                routerLink: "/login"
-            });
-            return response.json(); // parses JSON response into native JavaScript objects
+                if (res.ok) {
+                    // Registration successful, redirect to login page
+                    this.$router.push({ path: '/login' });
+                    return res.json(); // parses JSON response into native JavaScript objects
+                } else {
+                    // Handle registration error, show error message to the user
+                    console.error('Registration failed');
+                }
+            } catch (error) {
+                console.error('Error during registration:', error);
+            }
+
         }
     }
 };
